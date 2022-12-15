b6f49d5c120aa756c4ee5d663e6fd7b3510972bb
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-14 23:34:30 -0300

    ,

--- angular-auth/src/app/register/register.component.ts
+++ angular-auth/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from '../storage.service';
+import { RegisterService } from '../register.service';
 
 @Component({
   selector: 'app-register',
@@ -13,32 +14,43 @@ export class RegisterComponent implements OnInit {
   password: string = '';
   confirmation: string = '';
   registerFailMessage: string = '';
+  fullname: string = '';
 
   gender: string = '';
   photo: string = '';
+  messageReturn: string = '';
 
   constructor(
-    //private registerService: RegisterService,
+    private registerService: RegisterService,
     private router: Router,
-    private localStorage: StorageService
-    ) { }
+    private localStorage: StorageService,
+    ) {}
 
   ngOnInit(): void {
   }
-  register(){
-    /*
-    - Cadastrar usuário
-    - Fazer upload da foto OU informar link para a foto
-    - Verificar se a senha foi digitada igualmente no campo de verificação
-    - Não permitir cadastro de usuário com email já existente
-    */
+
+  register(): void {
+    try {
+      if (!this.mail || !this.name || !this.gender || !this.name || !this.password || !this.fullname)
+        throw new Error('Write all camps.');
+
+      if (this.password != this.confirmation)
+        throw new Error('Passwords are not equals.');
 
 
-    if(this.password != this.confirmation){
-      this.registerFailMessage = 'Distinct passwords. Please try again.';
-    }else{
-      this.router.navigate(['/dashboard']);
+
+      this.registerService.register(this.mail, this.name, this.fullname, this.gender, this.password, this.photo).subscribe((user) => {
+        this.router.navigate(['/']);
+      }, () => {
+        this.messageReturn =  'Not is possible make ragister. Try again later.';
+      });
+
+      this.messageReturn = 'Register sucess.';
+
+    }catch (ex: any) {
+      this.messageReturn = ex;
     }
+
   }
 
 }
