c9ed90726bff72e068dc3ccd64aaa13797ed0129
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-14 23:34:13 -0300

    .

--- angular-auth/src/app/login.service.ts
+++ angular-auth/src/app/login.service.ts
@@ -1,21 +1,37 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  private API_USER_ME: string = 'http://localhost:8080/api/user/me';
+  private API_USER_LOGIN: string = 'http://localhost:8080/api/user/login';
 
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string) {
+  /*login(username: string, password: string) {
     const headers = new HttpHeaders(
       {
-        authorization : 'Basic ' + btoa(username + ':' + password)
+        authorization : 'Basic' + username + ':' + password
       }
     );
+
     return this.http.get<Object>(this.API_USER_ME, {headers: headers});
+  }*/
+
+  login(mail: string, password: string) {
+
+    const httpOptions = {
+      headers: { authorization: 'Basic' + mail + ':' + password}
+    };
+
+    return this.http.get<User>(this.API_USER_LOGIN + `/${mail}`).pipe(
+      tap((response: User) => {
+        return response
+      })
+    );
   }
 }
