c7ae2b02405f09c52c7e0ab1304b9eb226b647f2
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-14 23:34:43 -0300

    .

--- springjpa/src/main/java/br/ufc/web/springrest01/rest/UserDTO.java
+++ springjpa/src/main/java/br/ufc/web/springrest01/rest/UserDTO.java
@@ -5,17 +5,32 @@ public class UserDTO {
     private int id;
     private String username;
     private String fullname;
+    private String password;
+    private String photo;
+    private String gender;
 
-    public UserDTO(String username2, String fullname2) {
-        this.username = username2;
-        this.fullname = fullname2;
+    public UserDTO(String username, String password, String fullname, String mail, String photo, String gender) {
+        this.username = username;
+        this.fullname = fullname;
+        this.fullname = password;
+        this.fullname = photo;
+        this.gender = gender;
     }
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
+    
     public String getUsername() {
         return username;
     }
@@ -29,5 +44,18 @@ public class UserDTO {
         this.fullname = fullname;
     }
     
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPhoto(){
+        return photo;
+    }
+    public void setPhoto(String photo){
+        this.photo = photo;
+    }
     
 }
--- springjpa/src/main/java/br/ufc/web/springrest01/rest/UserRestController.java
+++ springjpa/src/main/java/br/ufc/web/springrest01/rest/UserRestController.java
@@ -1,10 +1,11 @@
 package br.ufc.web.springrest01.rest;
 
-import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,50 +23,60 @@ public class UserRestController {
 
     @Autowired
     UserRepository userRepository;
-    
-    @GetMapping("/me")
-    Principal getMet(Principal me){
-        return me;
+
+    @GetMapping("/login/{mail}")
+    Optional<UserAccount> login(@PathVariable("mail") String mail){
+        //UserAccount result = new UserAccount("admin", "admin", "admin", "admin@admin", "https://cajamar.sp.gov.br/noticias/wp-content/uploads/sites/2/2021/07/site-vacinacao-33-anos.png");
+        
+        return userRepository.findUsersByMail(mail);
     }
 
-    /*@GetMapping("/me")
-    UserAccount getUserByIdAndPassword(Principal me){
-        String auth = me.toString();
-        String array[] = new String[2];
-        array = auth.split(":");
-
-        String username = array[0];
-        String password = array[1];
-
-        UserAccount userAux = new UserAccount("nulo", "nulo", "nulo");
+    @GetMapping("/me")
+    UserAccount getUserByIdAndPassword(@PathVariable String me){
+        UserAccount userAux = new UserAccount("nulo", "nulo", "nulo", "nulo@nulo", "nulo", "nulo");
         
-        Optional<UserAccount> result =  userRepository.findUserByUsername(username);
+        String array[] = new String[2];
+        array = me.split(":");
+
+        Optional<UserAccount> result =  userRepository.findUserByUsername(array[0]);
         if(result.isPresent()){
             userAux = result.get();
-            if(userAux.getPassword() == password){
+            if(userAux.getPassword() == array[1]){
                 return userAux;
             }
         }
         return userAux;
-    }*/
-
-    @GetMapping
-    Iterable<UserAccount> getUsers(@RequestParam Map<String, String> query){
-        Iterable<UserAccount> findAll = userRepository.findAll();
-        return findAll;
     }
 
+    @GetMapping("/{mail}")
+    Optional<UserAccount> getUser(@PathVariable String mail){
+        //UserAccount result = new UserAccount("admin", "admin", "admin", "admin@admin", "https://cajamar.sp.gov.br/noticias/wp-content/uploads/sites/2/2021/07/site-vacinacao-33-anos.png");
+        
+        Optional<UserAccount> result = userRepository.findUsersByMail(mail);
+
+        return result;
+        //Optional<UserAccount> result =  userRepository.findUserByMail("admin@admin");
+        //return !result.isPresent() ? null : result.get();
+    }
+
+    @GetMapping
+    Iterable<UserAccount> getUsers(){
+        Iterable<UserAccount> result = userRepository.findAll();
+        return result;
+    }
+
+    /*
     @GetMapping("/{id}")
     UserAccount getUser(@PathVariable int id){
         Optional<UserAccount> result =  userRepository.findById(id);
         return !result.isPresent() ? null : result.get();
-    }
+    }*/
    
     @PostMapping
     UserDTO addUser(@RequestBody UserAccount user) {
         user.setPassword(user.getPassword());
         UserAccount savedUser = userRepository.save(user);
-        return new UserDTO(savedUser.getUsername(), savedUser.getFullname());
-    }
 
+        return new UserDTO(savedUser.getUsername(), savedUser.getPassword(), savedUser.getFullname(), savedUser.getMail(),savedUser.getPhoto(), savedUser.getGender() );
+    }
 }
