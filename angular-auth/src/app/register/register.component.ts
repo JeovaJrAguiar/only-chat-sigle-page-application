54c3731f4f5acf82f145eb5c13e74b14a7898d65
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-11 22:59:10 -0300

    .

--- angular-auth/src/app/register/register.component.ts
+++ angular-auth/src/app/register/register.component.ts
@@ -9,6 +9,7 @@ import { RegisterService } from '../register.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  errorInRegister: number = 0;
   mail: string = '';
   name: string = '';
   password: string = '';
@@ -31,22 +32,23 @@ export class RegisterComponent implements OnInit {
 
   register(): void {
     try {
-      if (!this.mail || !this.name || !this.gender || !this.name || !this.password || !this.fullname)
-        throw new Error('Write all camps.');
+      if(this.name != '' || this.fullname != ''|| this.mail != '' || this.gender != '' || this.password != '' || this.confirmation != ''){
+        this.messageReturn = 'Write all camps.';
+        this.errorInRegister = 1;
+      }
 
-      if (this.password != this.confirmation)
-        throw new Error('Passwords are not equals.');
-
-
-
-      this.registerService.register(this.mail, this.name, this.fullname, this.gender, this.password, this.photo).subscribe((user) => {
-        this.router.navigate(['/']);
-      }, () => {
-        this.messageReturn =  'Not is possible make ragister. Try again later.';
-      });
-
-      this.messageReturn = 'Register sucess.';
-
+      if (this.password != this.confirmation){
+        this.messageReturn = 'Passwords are not equals.';
+        this.errorInRegister = 1;
+      }else {
+        this.registerService.register(this.mail, this.name, this.fullname, this.gender, this.password, this.photo).subscribe((user) => {
+          this.router.navigate(['/']);
+        }, () => {
+          this.messageReturn =  'Not is possible make ragister. Try again later.';
+        });
+  
+        this.messageReturn = 'Register sucess.';
+      }
     }catch (ex: any) {
       this.messageReturn = ex;
     }
