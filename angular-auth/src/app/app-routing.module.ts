e2f9a3c95c998510c94f45120f0c73a1d1b6fcb0
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-02-01 15:07:28 -0300

    template-image-rem
    concluido o card que apresenta o remetente da conversa

--- angular-auth/src/app/app-routing.module.ts
+++ angular-auth/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticatedGuard } from './authenticated.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -14,10 +10,7 @@ const routes: Routes = [
   { path: 'chat/:mailUserRem', component: ChatComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticatedGuard]},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
